bf9e2e79e6e223cfc3765155a3e80f70b3c804a1
Gabriel Willms <willms75@gmail.com>
2024-03-05 15:07:29 -0300

    Adicionando ignite-feed

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { PencilSimpleLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+              className={styles.cover}             
+              src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+              alt="logo Rocketseat"
+            />
+
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/gwillms.png" />
+
+                <strong>Gabriel Willms</strong>
+                <span>Full Stack Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                  <PencilSimpleLine size={20} />
+                  Editar seu Perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
